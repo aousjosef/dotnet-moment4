7b5dd1a23c804d035e2b9a8ea6c8cdcd85eed24a
aousjosef <aoussabah@gmail.com>
2024-02-21 00:48:11 +0100

    improved post method. image gets updated, old image gets deleted. New GUID is created and saved.

--- Moment 4/Controllers/SongsController.cs
+++ Moment 4/Controllers/SongsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 using Moment_4.Data;
 using Moment_4.Models;
 
@@ -43,16 +44,59 @@ namespace Moment_4.Controllers
         }
 
         // PUT: api/Songs/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutSong(int id, Song song)
+        /*OBS Song song och ImageFilePath är vad POST request förväntas tas emot. Dvs två olika inputs. Därför måste song.ImageFilePath = imagePath.
+         Ytterligare, så måste [FromForm] användas för att kombinera olika typer av data. Egentligen borde imagefilepath borde vara imagefile.*/
+        public async Task<IActionResult> PutSong(int id, [FromForm] Song song, [FromForm] IFormFile ImageFilePath)
         {
+
+
             if (id != song.Id)
             {
-                return BadRequest();
+                return BadRequest("The ID in the URL does not match the ID of the song.");
             }
 
-            _context.Entry(song).State = EntityState.Modified;
+            //Hämta låten som har valts, spara data i en var.
+            var selectedSongById = await _context.Songs.FindAsync(id);
+
+            //kontrollerar att det finns en fil som har blivit uppladdad
+            if (ImageFilePath != null && ImageFilePath.Length > 0)
+            {
+
+                // Först kontrollerar att selectedSongById.imagefilepath (Directory/GUID.png) är inte tom. Sen kontrollerar (Directory/GUID.png) finns redan i vår server.
+                if (!string.IsNullOrEmpty(selectedSongById.ImageFilePath) && System.IO.File.Exists(selectedSongById.ImageFilePath))
+                {
+                    //Om true, raderas bilden enlign nedan.
+
+                    System.IO.File.Delete(selectedSongById.ImageFilePath);
+                }
+
+
+                //Genererar unik filpath för den nya uppladdad bilden. Lik den för laravel för att sedan spara den i wwwrootes/images. Unika koden genereras mha GUID, och ersätter original bildens namn.
+                //Path.Combine tar 3 parametrar (1. Directory /var/www/myapp , 2.wwwroot/images , 3. GUID + (jpg,png,svg...)
+                var imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", Guid.NewGuid().ToString() + Path.GetExtension(ImageFilePath.FileName));
+
+                using (var stream = new FileStream(imagePath, FileMode.Create))
+                {
+                    await ImageFilePath.CopyToAsync(stream);
+                    selectedSongById.ImageFilePath = imagePath; // Set the file path on the song object
+                }
+            }
+
+            else
+            {
+                // If no new image file is uploaded, keep the path of the existing image.
+                song.ImageFilePath = selectedSongById.ImageFilePath;
+            }
+
+            //Då vi baserar hela nya datan på den selectedSongById.song. så fyller vi den med Song från input
+            selectedSongById.Name = song.Name;
+            selectedSongById.LengthInSeconds = song.LengthInSeconds;
+            selectedSongById.CategoryId = song.CategoryId;
+
+
+            _context.Entry(selectedSongById).State = EntityState.Modified;
 
             try
             {
@@ -70,11 +114,21 @@ namespace Moment_4.Controllers
                 }
             }
 
-            return NoContent();
+            //Return ok retunerar kod 200. mer avnändbar. 
+            return Ok(new
+            {
+                Message = $"The song with the name of {selectedSongById.Name} was updated successfullys.",
+                Song = selectedSongById
+            });
+
+
         }
 
         // POST: api/Songs
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+
+        /*OBS Song song och ImageFilePath är vad POST request förväntas tas emot. Dvs två olika inputs. Därför måste song.ImageFilePath = imagePath.
+            Ytterligare, så måste [FromForm] användas för att kombinera olika typer av data*/
+
         [HttpPost]
         public async Task<ActionResult<Song>> PostSong([FromForm] Song song, [FromForm] IFormFile ImageFilePath)
         {
